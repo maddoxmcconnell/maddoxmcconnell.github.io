8d4b41e0671318c3f4f9422e8ccfee8d35f9e623
maddoxmcconnell <maddox.mango@gmail.com>
2025-12-12 16:19:07 +0000

    fini9shed runtime

--- fsd-projects/runtime/js/levelData.js
+++ fsd-projects/runtime/js/levelData.js
@@ -36,7 +36,7 @@ var makeLevelData = function (window) {
           { type: "obstacle", x: 1300, y: groundY - 20, damage : 10, hitZone: 20, image: "img/DwarvenSpider.webp", offsetX: -38, offsetY: -25, scaleX: 0.08, scaleY: 0.08, speed: -2},
           { type: "enemy", x: 500, y: groundY - 20, damage: 20, score: 100, hitZone: 30, image: "img/Minotaur.png", offsetX: -155, offsetY: -145, scaleX: 0.5, scaleY: 0.5, speed: 2 },
           { type: "enemy", x: 1000, y: groundY - 20, damage: 20, score: 100, hitZone: 30, image: "img/Minotaur.png", offsetX: -155, offsetY: -145, scaleX: 0.5, scaleY: 0.5, speed: 2 },
-          { type: "enemy", x: 1500, y: groundY - 20, damage: 50, score: 1000, hitZone: 50, image: "img/LordDragon.png", offsetX: -255, offsetY: -450, scaleX: 0, scaleY: 1, speed: 2 },
+          { type: "enemy", x: 1500, y: groundY - 20, damage: 50, score: 1000, hitZone: 50, image: "img/LordDragon.png", offsetX: -200, offsetY: -300, scaleX: 0.8, scaleY: 0.8, speed: 2 },
           { type: "reward", x: 650, y: groundY - 90, hitZone: 25, image: "img/healpotion.png", offsetX: -35, offsetY: -35, scaleX: 0.15, scaleY: 0.15, score: 200, health: 5 },
           { type: "reward", x: 1150, y: groundY - 90, hitZone: 25, image: "img/healpotion.png", offsetX: -35, offsetY: -35, scaleX: 0.15, scaleY: 0.15, score: 200, health: 5 },
           { type: "levelmarker", x: 1800, y: groundY - 75, health: 100, hitZone: 25, image: "img/Morrowinddoor-removebg-preview.png", offsetX: -110, offsetY: -160, scaleX: 0.6, scaleY: 0.6, speed: 2},
